0a3218f4b0a82e4750ab56bf920636bd327446f0
Ehab-Tarek <ehabtarek144@gmail.com>
2025-01-30 19:56:51 +0200

    add middleware for request validation, security, and compression; update package dependencies

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,27 +3,10 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Get('hello')
-  getHello2(){
-    return {
-      message: 'Hello World!',
-      date: new Date(),
-
-    }
-  }
-
-  @Post('hello')
-  postHello(@Body() body){
-    return {
-      body,
-
-    }
-  }
-}
+} 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
+import helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ extended: true }));
+  app.use(helmet());
+  app.use(compression());
+
+  app.enableCors();
+
 }
 bootstrap();
